29e302b56125614e7fd1010c1634e11d0d829b30
Jason Holt Smith <jason.smith@verizonconnect.com>
2019-11-15 10:30:07 +0000

    adding missing package main

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/* containers */
+export * from './containers/selenium-facet-provider';
+export * from './containers/selenium-facet';
+/* sessions-browserstack */
+export * from './sessions/browserstack/browserstack-session';
+export * from './sessions/browserstack/browserstack-session-generator';
+export * from './sessions/browserstack/configuration/browserstack-config';
+/* helpers */
+export * from './helpers/facet-locator-converter';
