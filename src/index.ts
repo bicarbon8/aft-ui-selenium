485829c373b85002e7eb6b3247ac175cce4e54f9
Jason Holt Smith <jason.smith@verizonconnect.com>
2019-11-21 15:55:58 +0000

    adding new classes to exports from package

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,15 @@ export * from './containers/selenium-facet';
 export * from './sessions/browserstack/browserstack-session';
 export * from './sessions/browserstack/browserstack-session-generator';
 export * from './sessions/browserstack/configuration/browserstack-config';
+/* sessions-sauce-labs */
+export * from './sessions/sauce-labs/sauce-labs-session';
+export * from './sessions/sauce-labs/sauce-labs-session-generator';
+export * from './sessions/sauce-labs/configuration/sauce-labs-config';
+/* sessions-selenium-grid */
+export * from './sessions/selenium-grid/abstract-grid-session';
+export * from './sessions/selenium-grid/selenium-grid-session';
+export * from './sessions/selenium-grid/selenium-grid-session-generator';
+export * from './sessions/selenium-grid/configuration/selenium-grid-config';
 /* helpers */
 export * from './helpers/facet-locator-converter';
+export * from './helpers/build-name';
